6693669338a7b51af3634bd768cba4462d96b172
Danilo Silva <danilo.dos.s.silva@gmail.com>
2021-07-06 18:16:33 -0300

    'esqueleto' da página tema ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
+import { TemaComponent } from './tema/tema.component';
 
 
 //OBJETOS DE ROTAS
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: '', redirectTo: 'login', pathMatch: 'full'}, // Como default é a página inicial
   {path: 'login', component: LoginComponent},
   /*{path: 'cadastrar', component: CadastrarComponent} NÃO PRECISA PORQUE O 'CADASTRAR' É UMA JANELA MODAL NO COMPONENTE LOGIN*/
-  {path: 'inicio', component: InicioComponent}
+  {path: 'inicio', component: InicioComponent}, // página de inicio ou Home
+  {path: 'tema', component: TemaComponent}
 ];
 
 @NgModule({
--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http'; // Disponibiliza a criação de objetos que usem CRUD 
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Tema } from '../model/Tema';
 
 @Component({
   selector: 'app-tema',
@@ -7,9 +11,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemaComponent implements OnInit {
 
-  constructor() { }
+  constructor( 
+    // private route: RouterLink, 
+    private http: HttpClient // Disponibiliza os métodos HTTP get, post, put e delete)
+    ) { }
+  
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {}
 }
