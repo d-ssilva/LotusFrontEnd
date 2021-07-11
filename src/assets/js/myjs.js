84cfb56b14e83be023263f49f650a5ad8ca8d316
isaquec <isaquedeco@hotmail.com>
2021-07-11 14:28:35 -0300

    menu responsivo

---
+++ src/assets/js/myjs.js
@@ -0,0 +1,9 @@
+
+    const blogin = document.querySelector('.bblogin');
+    const bcadastro = document.querySelector('.bbcadastro');
+    const formBx = document.querySelector('.formBx');
+    
+    bbcadastro.onclick = function () {
+      formBx.classList.add('active')
+    }
+ 
