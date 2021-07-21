d450c2eb55be56dd7a04bf07eee1bd9da685f537
isaquec <isaquedeco@hotmail.com>
2021-07-21 19:21:24 -0300

    implementando melhores e piores impresas

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -13,6 +13,8 @@ export class AuthService {
   constructor(
     private http: HttpClient
   ) { }
+
+
 
   entrar(userLogin: UserLogin): Observable<UserLogin> {
     return this.http.post<UserLogin>('https://apilotus.herokuapp.com/usuario/logar', userLogin)
